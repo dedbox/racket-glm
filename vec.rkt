089b310d71960e35eb64096c6439d17b02805f3a
Eric Griffis <dedbox@gmail.com>
2019-09-22 11:49:24 -0700

    merge vec2, vec3, vec4 into vec module

--- vec.rkt
+++ vec.rkt
@@ -1,12 +1,13 @@
 #lang racket/base
 
 ;;; ----------------------------------------------------------------------------
-;;; Vector Base
+;;; Abstract Vector
 
 (require ffi/unsafe
          racket/flonum
          racket/function
          racket/generic
+         racket/list
          racket/pretty
          racket/sequence
          (for-syntax racket/base
@@ -104,3 +105,375 @@
 (define (--vec v) (vec-= v ((vec-constructor v) 1.0)) v)
 (define (vec++ v) (begin0 (vec-copy v) (++vec v)))
 (define (vec-- v) (begin0 (vec-copy v) (--vec v)))
+
+;;; ============================================================================
+;;; 
+;;; 2-D Vector
+;;;
+;;; glm/detail/type_vec2.hpp
+;;; glm/detail/type_vec2.inl
+
+(struct vec2 glm:vec ()
+  #:name glm:vec2
+  #:constructor-name make-vec2
+  #:methods gen:glm-vec
+  [(define (vec-name _) 'vec2)
+   (define (vec-predicate _) vec2?)
+   (define (vec-constructor _) vec2)])
+
+(define vec2
+  (case-lambda
+    [() (vec2 0.0)]
+    [(a) (if (vec2? a) (vec-copy a) (vec2 a a))]
+    [(x y) (make-vec2 (make-vec-data x y))]))
+
+(define x-dir2 (vec2 1.0 0.0))
+(define y-dir2 (vec2 0.0 1.0))
+(define z-dir2 (vec2 0.0 0.0))
+(define w-dir2 (vec2 0.0 0.0))
+
+(define  zero2 (vec2 0.0 0.0))
+(define   one2 (vec2 1.0 1.0))
+(define   two2 (vec2 2.0 2.0))
+(define three2 (vec2 3.0 3.0))
+(define  four2 (vec2 4.0 4.0))
+(define  five2 (vec2 5.0 5.0))
+(define   six2 (vec2 6.0 6.0))
+(define seven2 (vec2 7.0 7.0))
+(define eight2 (vec2 8.0 8.0))
+(define  nine2 (vec2 9.0 9.0))
+
+(define   -one2 (vec2 -1.0 -1.0))
+(define   -two2 (vec2 -2.0 -2.0))
+(define -three2 (vec2 -3.0 -3.0))
+(define  -four2 (vec2 -4.0 -4.0))
+(define  -five2 (vec2 -5.0 -5.0))
+(define   -six2 (vec2 -6.0 -6.0))
+(define -seven2 (vec2 -7.0 -7.0))
+(define -eight2 (vec2 -8.0 -8.0))
+(define  -nine2 (vec2 -9.0 -9.0))
+
+;;; ............................................................................
+
+(module+ test
+  (require racket/function
+           rackunit)
+
+  (test-case "equal?"
+    (check equal? zero2 zero2)
+    (check equal? one2 one2)
+    (check (negate equal?) one2 zero2)
+    (check (negate equal?) zero2 one2))
+
+  (test-case "vec+="
+    (define v (vec2 zero2))
+    (check equal? v zero2) (vec+= v one2)
+    (check equal? v one2) (vec+= v one2)
+    (check equal? v two2))
+
+  (test-case "vec-="
+    (define v (vec2 one2))
+    (check equal? v one2) (vec-= v one2)
+    (check equal? v zero2) (vec-= v one2)
+    (check equal? v -one2))
+
+  (test-case "vec*="
+    (define v (vec2 one2))
+    (check equal? v one2) (vec*= v one2)
+    (check equal? v one2) (vec*= v two2)
+    (check equal? v two2) (vec*= v two2)
+    (check equal? v four2))
+
+  (test-case "vec/="
+    (define v (vec2 one2))
+    (check equal? v (vec2 1.0)) (vec/= v two2)
+    (check equal? v (vec2 0.5)) (vec/= v five2)
+    (check equal? v (vec2 0.1)))
+
+  (test-case "vec+"
+    (check equal? (vec+ one2) one2)
+    (check equal? (vec+ one2 one2) two2)
+    (check equal? (vec+ one2 one2 one2) three2))
+
+  (test-case "vec-"
+    (check equal? (vec- zero2) zero2)
+    (check equal? (vec- one2 zero2) one2)
+    (check equal? (vec- one2) -one2)
+    (check equal? (vec- one2 one2) zero2)
+    (check equal? (vec- one2 one2 one2) -one2)
+    (check equal? (vec- one2 one2 one2 one2) -two2))
+
+  (test-case "vec*"
+    (check equal? (vec* zero2) zero2)
+    (check equal? (vec* one2) one2)
+    (check equal? (vec* two2) two2)
+    (check equal? (vec* two2 three2) six2)
+    (check equal? (vec* two2 three2 four2) (vec2 24.0 24.0))
+    (check equal? (vec* (vec2 1.0 2.0)
+                        (vec2 2.0 3.0)
+                        (vec2 3.0 4.0)) (vec2 6.0 24.0)))
+
+  (test-case "vec/"
+    (check equal? (vec/ one2) one2)
+    (check equal? (vec/ zero2 one2) zero2)
+    (check equal? (vec/ one2 two2) (vec2 0.5 0.5))
+    (check equal? (vec/ one2 two2 two2) (vec2 0.25 0.25))
+    (check equal? (vec/ one2 two2 two2 two2) (vec2 0.125 0.125))))
+
+;;; ============================================================================
+;;; 
+;;; 3-D Vector
+;;;
+;;; glm/detail/type_vec3.hpp
+;;; glm/detail/type_vec3.inl
+
+(struct vec3 glm:vec ()
+  #:name glm:vec3
+  #:constructor-name make-vec3
+  #:methods gen:glm-vec
+  [(define (vec-name _) 'vec3)
+   (define (vec-predicate _) vec3?)
+   (define (vec-constructor _) vec3)])
+
+(define vec3
+  (case-lambda
+    [() (vec3 0.0)]
+    [(a) (cond [(vec4? a) (apply vec3 (drop-right (vec->list a) 1))]
+               [(vec3? a) (vec-copy a)]
+               [else (vec3 a a a)])]
+    [(x y z) (make-vec3 (make-vec-data x y z))]))
+
+(define x-dir3 (vec3 1.0 0.0 0.0))
+(define y-dir3 (vec3 0.0 1.0 0.0))
+(define z-dir3 (vec3 0.0 0.0 1.0))
+(define w-dir3 (vec3 0.0 0.0 0.0))
+
+(define  zero3 (vec3 0.0 0.0 0.0))
+(define   one3 (vec3 1.0 1.0 1.0))
+(define   two3 (vec3 2.0 2.0 2.0))
+(define three3 (vec3 3.0 3.0 3.0))
+(define  four3 (vec3 4.0 4.0 4.0))
+(define  five3 (vec3 5.0 5.0 5.0))
+(define   six3 (vec3 6.0 6.0 6.0))
+(define seven3 (vec3 7.0 7.0 7.0))
+(define eight3 (vec3 8.0 8.0 8.0))
+(define  nine3 (vec3 9.0 9.0 9.0))
+
+(define   -one3 (vec3 -1.0 -1.0 -1.0))
+(define   -two3 (vec3 -2.0 -2.0 -2.0))
+(define -three3 (vec3 -3.0 -3.0 -3.0))
+(define  -four3 (vec3 -4.0 -4.0 -4.0))
+(define  -five3 (vec3 -5.0 -5.0 -5.0))
+(define   -six3 (vec3 -6.0 -6.0 -6.0))
+(define -seven3 (vec3 -7.0 -7.0 -7.0))
+(define -eight3 (vec3 -8.0 -8.0 -8.0))
+(define  -nine3 (vec3 -9.0 -9.0 -9.0))
+
+;;; ............................................................................
+
+(module+ test
+
+  (test-case "equal?"
+    (check equal? zero3 zero3)
+    (check equal? one3 one3)
+    (check (negate equal?) one3 zero3)
+    (check (negate equal?) zero3 one3))
+
+  (test-case "vec+="
+    (define v (vec3 zero3))
+    (check equal? v zero3) (vec+= v one3)
+    (check equal? v one3) (vec+= v one3)
+    (check equal? v two3))
+
+  (test-case "vec-="
+    (define v (vec3 one3))
+    (check equal? v one3) (vec-= v one3)
+    (check equal? v zero3) (vec-= v one3)
+    (check equal? v -one3))
+
+  (test-case "vec*="
+    (define v (vec3 one3))
+    (check equal? v one3) (vec*= v one3)
+    (check equal? v one3) (vec*= v two3)
+    (check equal? v two3) (vec*= v two3)
+    (check equal? v four3))
+
+  (test-case "vec/="
+    (define v (vec3 one3))
+    (check equal? v (vec3 1.0)) (vec/= v two3)
+    (check equal? v (vec3 0.5)) (vec/= v five3)
+    (check equal? v (vec3 0.1)))
+
+  (test-case "vec+"
+    (check equal? (vec+ one3) one3)
+    (check equal? (vec+ one3 one3) two3)
+    (check equal? (vec+ one3 one3 one3) three3))
+
+  (test-case "vec-"
+    (check equal? (vec- zero3) zero3)
+    (check equal? (vec- one3 zero3) one3)
+    (check equal? (vec- one3) -one3)
+    (check equal? (vec- one3 one3) zero3)
+    (check equal? (vec- one3 one3 one3) -one3)
+    (check equal? (vec- one3 one3 one3 one3) -two3))
+
+  (test-case "vec*"
+    (check equal? (vec* zero3) zero3)
+    (check equal? (vec* one3) one3)
+    (check equal? (vec* two3) two3)
+    (check equal? (vec* two3 three3) six3)
+    (check equal? (vec* two3 three3 four3) (vec3 24.0 24.0 24.0))
+    (check equal? (vec* (vec3 1.0 2.0 3.0)
+                        (vec3 2.0 3.0 4.0)
+                        (vec3 3.0 4.0 5.0)) (vec3 6.0 24.0 60.0)))
+
+  (test-case "vec/"
+    (check equal? (vec/ one3) one3)
+    (check equal? (vec/ zero3 one3) zero3)
+    (check equal? (vec/ one3 two3) (vec3 0.5 0.5 0.5))
+    (check equal? (vec/ one3 two3 two3) (vec3 0.25 0.25 0.25))
+    (check equal? (vec/ one3 two3 two3 two3) (vec3 0.125 0.125 0.125))))
+
+;;; ============================================================================
+;;; 
+;;; 4-D Vector
+;;;
+;;; glm/detail/type_vec4.hpp
+;;; glm/detail/type_vec4.inl
+
+(struct vec4 glm:vec ()
+  #:name glm:vec4
+  #:constructor-name make-vec4
+  #:methods gen:glm-vec
+  [(define (vec-name _) 'vec4)
+   (define (vec-predicate _) vec4?)
+   (define (vec-constructor _) vec4)])
+
+(define vec4
+  (case-lambda
+    [() (vec4 0.0)]
+    [(a) (if (vec4? a) (vec-copy a) (vec4 a a a a))]
+    [(a b) (cond [(vec3? a) (apply (curryr vec4 b) (vec->list a))]
+                 [(vec3? b) (apply (curry vec4 a) (vec->list b))]
+                 [else (raise-argument-error 'vec4 "a scalar or vec3" 0 a b)])]
+    [(x y z w) (make-vec4 (make-vec-data x y z w))]))
+
+(define x-dir4 (vec4 1.0 0.0 0.0 0.0))
+(define y-dir4 (vec4 0.0 1.0 0.0 0.0))
+(define z-dir4 (vec4 0.0 0.0 1.0 0.0))
+(define w-dir4 (vec4 0.0 0.0 0.0 1.0))
+
+(define-values (zero4 one4 two4 three4 four4 five4 six4 seven4 eight4 nine4)
+  (apply values (for/list ([k (in-range 10)]) (vec4 (real->single-flonum k)))))
+
+(define-values (-one4 -two4 -three4 -four4 -five4 -six4 -seven4 -eight4 -nine4)
+  (apply values (for/list ([k (in-range 1 10)]) (vec4 (- (real->single-flonum k))))))
+
+;;; ............................................................................
+
+(module+ test
+
+  ;; prototyping tests
+
+  (test-case "equal?"
+    (check equal? zero4 zero4)
+    (check equal? one4 one4)
+    (check (negate equal?) one4 zero4)
+    (check (negate equal?) zero4 one4))
+
+  (test-case "vec+="
+    (define v (vec4 zero4))
+    (check equal? v zero4) (vec+= v one4)
+    (check equal? v one4) (vec+= v one4)
+    (check equal? v two4))
+
+  (test-case "vec-="
+    (define v (vec4 one4))
+    (check equal? v one4) (vec-= v one4)
+    (check equal? v zero4) (vec-= v one4)
+    (check equal? v -one4))
+
+  (test-case "vec*="
+    (define v (vec4 one4))
+    (check equal? v one4) (vec*= v one4)
+    (check equal? v one4) (vec*= v two4)
+    (check equal? v two4) (vec*= v two4)
+    (check equal? v four4))
+
+  (test-case "vec/="
+    (define v (vec4 one4))
+    (check equal? v (vec4 1.0)) (vec/= v two4)
+    (check equal? v (vec4 0.5)) (vec/= v five4)
+    (check equal? v (vec4 0.1)))
+
+  (test-case "vec+"
+    (check equal? (vec+ one4) one4)
+    (check equal? (vec+ one4 one4) two4)
+    (check equal? (vec+ one4 one4 one4) three4))
+
+  (test-case "vec-"
+    (check equal? (vec- zero4) zero4)
+    (check equal? (vec- one4 zero4) one4)
+    (check equal? (vec- one4) -one4)
+    (check equal? (vec- one4 one4) zero4)
+    (check equal? (vec- one4 one4 one4) -one4)
+    (check equal? (vec- one4 one4 one4 one4) -two4))
+
+  (test-case "vec*"
+    (check equal? (vec* zero4) zero4)
+    (check equal? (vec* one4) one4)
+    (check equal? (vec* two4) two4)
+    (check equal? (vec* two4 three4) six4)
+    (check equal? (vec* two4 three4 four4) (vec4 24.0 24.0 24.0 24.0))
+    (check equal? (vec* (vec4 1.0 2.0 3.0 4.0)
+                        (vec4 2.0 3.0 4.0 5.0)
+                        (vec4 3.0 4.0 5.0 6.0)) (vec4 6.0 24.0 60.0 120.0)))
+
+  (test-case "vec/"
+    (check equal? (vec/ one4) one4)
+    (check equal? (vec/ zero4 one4) zero4)
+    (check equal? (vec/ one4 two4) (vec4 0.5 0.5 0.5 0.5))
+    (check equal? (vec/ one4 two4 two4) (vec4 0.25 0.25 0.25 0.25))
+    (check equal? (vec/ one4 two4 two4 two4) (vec4 0.125 0.125 0.125 0.125)))
+
+  (test-case "vec++"
+    (define v (vec4 zero4))
+    (check equal? v zero4) (vec++ v)
+    (check equal? v one4) (vec++ v)
+    (check equal? v two4))
+
+  (test-case "vec--"
+    (define v (vec4 zero4))
+    (check equal? v zero4) (vec-- v)
+    (check equal? v -one4) (vec-- v)
+    (check equal? v -two4))
+
+  (test-case "vec+"
+    (check equal? zero4 (vec+ zero4))
+    (check equal? one4 (vec+ one4))
+    (check equal? two4 (vec+ one4 1.0))
+    (check equal? two4 (vec+ one4 one4))
+    (check equal? three4 (vec+ one4 1.0 one4))
+    (check equal? three4 (vec+ one4 two4)))
+
+  (test-case "vec-"
+    (check equal? zero4 (vec- zero4))
+    (check equal? -one4 (vec- one4))
+    (check equal? zero4 (vec- one4 1.0))
+    (check equal? zero4 (vec- one4 one4))
+    (check equal? -one4 (vec- one4 1.0 one4))
+    (check equal? -one4 (vec- one4 two4)))
+
+  (test-case "vec*"
+    (check equal? zero4 (vec* zero4))
+    (check equal? zero4 (vec* zero4 0.0))
+    (check equal? zero4 (vec* zero4 zero4))
+    (check equal? four4 (vec* two4 2.0))
+    (check equal?  six4 (vec* two4 three4))
+    (check equal?  six4 (vec* one4 2.0 three4)))
+
+  (test-case "vec/"
+    (check equal? one4 (vec/ one4))
+    (check equal? two4 (vec/ six4 3.0))
+    (check equal? two4 (vec/ six4 three4))
+    (check equal? one4 (vec/ six4 3.0 two4))))
